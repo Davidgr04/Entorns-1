b73ffb18858c90e20ba98d0f22ac6aec0f84d49e
unknown <davidgr2004ole@gmail.com>
2022-09-20 11:27:21 +0200

    actu2

---
+++ python/holamundo.py
@@ -0,0 +1,22 @@
+# Ejercicios de intro a Python
+# Ejercicio 1
+# a= 3+2
+# b= 2*5
+# c= a/b
+# print(c**2)
+# Ejercicio 2
+# prefix = "34"
+# telefon = "611494172"
+# extensio = "100"
+# print ("(+"+prefix+")-"+telefon+"."+extensio)
+# opcion dificil ejercicio 2
+# print("Digue'm el prefix")
+# prefix = input()
+# print("Digue'm el telefon")
+# telefon = input()
+# print("Digue'm l'extensio")
+# extensio = input()
+# print("El teu numero de telefon es el seguent:",prefix+telefon+extensio)
+# Exercici 3
+# nom = "David"
+# print(type(nom))
