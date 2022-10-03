4447f3e70ba615b7c6478aeca8b8f8b8d989f667
unknown <davidgr2004ole@gmail.com>
2022-10-03 09:52:39 +0200

    first commit

--- python/holamundo.py
+++ python/holamundo.py
@@ -1,12 +1,14 @@
 # Ejercicios de intro a Python
+
 # Ejercicio 1
 # a= 3+2
 # b= 2*5
 # c= a/b
 # print(c**2)
+
 # Ejercicio 2
 # prefix = "34"
-# telefon = "611494172"
+# telefon = "611494172" 
 # extensio = "100"
 # print ("(+"+prefix+")-"+telefon+"."+extensio)
 # opcion dificil ejercicio 2
@@ -17,6 +19,7 @@
 # print("Digue'm l'extensio")
 # extensio = input()
 # print("El teu numero de telefon es el seguent:",prefix+telefon+extensio)
+
 # Exercici 3
 # nom = "David"
 # print(type(nom))
